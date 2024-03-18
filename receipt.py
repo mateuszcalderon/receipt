28a3e0a3440cac83ee9d44d51dbc329d5f7395b2
Mateus Calderon Reis <115183635+mateuszjoestar@users.noreply.github.com>
2024-03-18 09:46:00 -0300

    Add files via upload

---
+++ receipt.py
@@ -0,0 +1,35 @@
+# Last update: 03/18/2024
+# importing datetime library we're able to work with dates and times.
+import datetime
+# datetime.datetime.now() is designed to return the current UTC date and time.
+current_time = datetime.datetime.now()
+product_price = float(input("PRICE: "))
+product_name = input("PRODUCT: ")
+quantity = int(input("QTY: "))
+print("---------------------------")
+print()
+print("          RECEIPT          ")
+# strftime convert object to a string according to a given format.
+# %m shows month as a zero-padded decimal number.
+# %d shows day of the month as a zero-padded decimal number.
+# %Y shows year with century as a decimal number.
+# %H shows hour (24-hour clock) as a zero-padded decimal number.
+# %M shows minute as a zero-padded decimal number.
+# %S shows second as a zero-padded decimal number.
+print(current_time.strftime("%m/%d/%Y.........%H:%M:%S"))
+print()
+# the .upper() method will return a string where all characters are in upper case.
+print(f"PRODUCT   : {product_name.upper()}")
+print(f"QTY       : {quantity}")
+print()
+sub_total = product_price * quantity
+print(f"SUB TOTAL : {sub_total}")
+print()
+print("7.125% TAX")
+tax = 7.125 / 100 * sub_total
+print(f"TAX AMOUNT: ${tax:.2f}")
+print()
+total = tax + sub_total
+print(f"TOTAL     : ${total:.2f}")
+print()
+print("---------------------------")
