d4087b848c1772b829bb2ce7805dcbf27092eba1
Mateus Calderon Reis <115183635+mateuszcalderon@users.noreply.github.com>
2024-04-12 11:02:31 -0300

    Update receipt.py

--- receipt.py
+++ receipt.py
@@ -1,15 +1,15 @@
-# LAST UPDATE: 03/18/2024.
-# importing datetime library we're able to work with dates and times.
+# LAST UPDATE: 04/12/2024.
+# Importing datetime library we're able to work with dates and times.
 import datetime
 # datetime.datetime.now() is designed to return the current UTC date and time.
 current_time = datetime.datetime.now()
-product_price = float(input("PRICE: "))
-product_name = input("PRODUCT: ")
-quantity = int(input("QTY: "))
+product_price = float(input("PRICE: "))  # Here the user will enter a price, e.g., 59.00
+product_name = input("PRODUCT: ")        # And then a product, e.g., Phone Case
+quantity = int(input("QTY: "))           # And finally a quantity, e.g., 1
 print("---------------------------")
 print()
 print("          RECEIPT          ")
-# strftime convert object to a string according to a given format.
+# strftime will convert object to a string according to the given format:
 # %m shows month as a zero-padded decimal number.
 # %d shows day of the month as a zero-padded decimal number.
 # %Y shows year with century as a decimal number.
@@ -18,12 +18,12 @@ print("          RECEIPT          ")
 # %S shows second as a zero-padded decimal number.
 print(current_time.strftime("%m/%d/%Y.........%H:%M:%S"))
 print()
-# the .upper() method will return a string where all characters are in upper case.
+# The .upper() method will return a string where all characters are in upper case.
 print(f"PRODUCT   : {product_name.upper()}")
 print(f"QTY       : {quantity}")
 print()
 sub_total = product_price * quantity
-print(f"SUB TOTAL : {sub_total}")
+print(f"SUB TOTAL : {sub_total:.2f}")
 print()
 print("7.125% TAX")
 tax = 7.125 / 100 * sub_total
